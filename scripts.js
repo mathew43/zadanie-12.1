a14c4d7d3aefecfa49d8d26868684b57dd1352c0
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2017-12-28 15:07:20 +0100

    add function getJoke

--- scripts.js
+++ scripts.js
@@ -6,3 +6,13 @@ button.addEventListener('click', function() {
 });
 
 var paragraph = document.getElementById('joke');
+
+function getJoke(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', url);
+	xhr.addEventListener('load', function(){
+		var response = JSON.parse(xhr.response);
+		paragraph.innerHTML = response.value.joke;
+	})
+	xhr.send();
+}
